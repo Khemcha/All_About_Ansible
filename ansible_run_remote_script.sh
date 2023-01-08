8f539e327e7b922d1de282f2bafbdfb0b1af2243
Khemnath <khemnath.devops@outlook.com>
2023-01-08 23:21:00 +0800

    Add new playbook

---
+++ ansible_run_remote_script.sh
@@ -0,0 +1,10 @@
+# This playbook show how to run the script in remote server. The script is present in controller node, it executes the script in remote location
+# It also passess 3 arguments to script.
+---
+- name: This playbook will execute a remote shell script.
+  hosts: web
+  gather_facts: false
+  tasks:
+  - name: Run a script test.sh and pass arguments and then print the arguments.
+    script:
+      cmd: test.sh One Two Three
