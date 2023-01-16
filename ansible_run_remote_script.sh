7a7e25ac6b69dd137d35f8bec7712c21714b453e
Khemnath <khemnath.devops@outlook.com>
2023-01-16 08:49:27 +0800

    Update the playbook to show ip, hostname

--- ansible_run_remote_script.sh
+++ ansible_run_remote_script.sh
@@ -1,5 +1,5 @@
 # This playbook show how to run the script in remote server. The script is present in controller node, it executes the script in remote location
-# It also passess 3 arguments to script.
+# It also passess 3 arguments to script and print accordingly.
 ---
 - name: This playbook will execute a remote shell script.
   hosts: web
